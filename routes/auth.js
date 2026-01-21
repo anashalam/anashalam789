83cf867a1ae9ecf210cc1e08c94f957f18bb5caa
Anash Alam <anashalam@gmail.com>
2026-01-21 13:19:07 +0530

    fixed db import

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
-const db = require('../config/database').default;
-
+const db = require('../config/database');
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET || 'SUPER_SECRET_KEY';
 
